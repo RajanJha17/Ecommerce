c427777071890c8f0ad011ffac10d14bb817d67f
Rajan Jha <rajanj448@gmail.com>
2025-08-24 10:12:28 +0530

    product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter product name"],
+        trim:true
+    },
+    price:{
+        type:Number,
+        required:[true,"Please enter product price"],
+        maxLength:[7,"Price must be less than 7 characters"]
+    },
+    description:{
+        type:String,
+        required:[true,"Please enter product description"]
+    },
+    ratings:{
+        type:Number,
+        default:0
+    },
+    image:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required:[true,"Please enter product category"]
+    },
+    stock:{
+        type:Number,
+        required:[true,"Please enter product stock"],
+        maxLength:[5,"Stock must be less than 5 characters"],
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type:Number,
+                required:true,
+                min:1,
+                max:5
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+const Product=mongoose.model("Product",productSchema);
+
+export default Product;
