7c0c54bbcbfafba2ac601ed87f154fd1ca4ae023
Rajan Jha <rajanj448@gmail.com>
2025-08-27 13:51:35 +0530

    cors frontend

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app=express();
 
 app.use(cors(
     {
-        origin: 'https://ecommerce-jiot.onrender.com',
+        origin: 'https://sika-jade.vercel.app',
         credentials: true,
     }
 ));
